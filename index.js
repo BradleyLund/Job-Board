0dcd423db23ed98a53253c9cd89c77efe68b9745
Bradley Lund <bradjlund@gmail.com>
2020-09-26 13:20:19 +0200

    map data wanted

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const Sheet = require('./sheet');
 const fetch = require('node-fetch');
  
 (async function() {
-    const sheet = new Sheet();
-    await sheet.load();
 
     const result = await fetch('https://jobs.github.com/positions.json?description=python&location=new+york');
     const json = await result.json();
 
-    console.log({json})
+    const rows = json.map(job => {
+        
+        return {
+            company: job.company,
+            title: job.title,
+            location: job.location,
+            date: job.created_at,
+            url: job.url
+        }
+    })
+
+    const sheet = new Sheet();
+    await sheet.load();
+    
+    
 })()
 
  
