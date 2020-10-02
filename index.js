e703d1af9ab49478b99c574f47ef2945aa8d1606
Bradley Lund <bradjlund@gmail.com>
2020-10-02 20:12:33 +0200

    multiple page scrape

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Sheet = require('./sheet');
 const fetch = require('node-fetch');
- 
-(async function() {
 
-    const result = await fetch('https://jobs.github.com/positions.json?&location=remote');
+async function scrapePage(i) {
+    const result = await fetch(`https://jobs.github.com/positions.json?page=${i}&search=remote`);
     const json = await result.json();
 
     const rows = json.map(job => {
@@ -17,12 +16,28 @@ const fetch = require('node-fetch');
         }
     })
 
+    return rows;
+
+    
+}
+ 
+(async function() {
+
+    let i = 1;
+    let rows = [];
+    while(true) {
+        const newRows = await scrapePage(i);
+        console.log('row had ',newRows.length );
+        if (newRows.length === 0 ) break;
+        rows = [...rows,...newRows];
+        
+        i++;
+    }
+
     const sheet = new Sheet();
     await sheet.load();
-
+    
     await sheet.addRows(rows)
-    
-    
 })()
 
  
